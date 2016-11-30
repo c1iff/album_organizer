8068fb6a3fb75d1f425165ccbb21780623b9837e
Chris Clifford and Jonathan Thom <jonathan@>
2016-11-30 14:07:51 -0800

    add all routes in app.rb

--- app.rb
+++ app.rb
@@ -16,3 +16,35 @@ get('/artists') do
   @artists = Artist.all()
   erb(:artists)
 end
+
+post('/artists') do
+  album_name = params.fetch('name')
+  Artist.new({:name => album_name}).save()
+  @artists = Artist.all()
+  erb(:success)
+end
+
+get('/artists/:id') do
+  @artist = Artist.find(params.fetch('id').to_i())
+  # @albums = Artist.find(params.fetch('id').to_i()).albums
+  erb(:artist)
+end
+
+get('/albums/:id') do
+  @albums = Album.find(params.fetch('id').to_i())
+  erb(:album)
+end
+
+get('/artists/:id/album/new') do
+  @artist = Artist.find(params.fetch('id').to_i())
+  erb(:artists_albums_form)
+end
+
+post('/albums') do
+  name = params.fetch('name')
+  @album = Album.new({:name => name})
+  @album.save()
+  @artist = Artist.find(params.fetch('artist_id').to_i())
+  @artist.add_album(@album)
+  erb(:success)
+end
